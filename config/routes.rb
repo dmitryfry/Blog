18b517111384589316303c724a01b78c202b29ee
Dmitry Fry <im@dmitryfry.com>
2017-01-03 20:07:08 +0300

    add pages#terms & about

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 resources :articles
 resource :contacts, only: [:new, :create], path_names: {:new => ''}
 
+get 'terms' => 'pages#terms'
+get 'about' => 'pages#about'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
