95aa69b23a7b44e08bd3a785f19b3ad343fe9ee7
Dmitry Fry <im@dmitryfry.com>
2017-01-06 15:57:24 +0300

    add username column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105145913) do
+ActiveRecord::Schema.define(version: 20170106124250) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -55,8 +55,10 @@ ActiveRecord::Schema.define(version: 20170105145913) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
