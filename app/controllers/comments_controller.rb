b94f2f93c6598d0bd32291b9a03b918f57d1d6a4
Dmitry Fry <im@dmitryfry.com>
2017-01-08 22:05:18 +0300

    add bootstrap

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,18 @@
 class CommentsController < ApplicationController
+
+  before_filter :authenticate_user!, :only => [:create]
+
   def create
     @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
+    @comments = @article.comments.new(comment_params)
+    @comments.author = current_user.username
+    @comments.save
 
     redirect_to article_path(@article)
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:author, :body)
+    params.require(:comment).permit(:body)
   end
 end
